1f4659c98d0d17ed3f9901e2b9fd73553824d85a
yeni <yeniyeni0224@gmail.com>
2025-03-22 18:25:26 +0900

    Revert "feat.CreateTitlePage"
    This reverts commit 94f2e952149a29d4cbed842ad2c74704cfe3033d.

--- BestConsoleGame/BestConsoleGame/BestConsoleGame.cpp
+++ BestConsoleGame/BestConsoleGame/BestConsoleGame.cpp
@@ -358,84 +358,14 @@ void DrawDungeonRect() {
         putchar('@');
     }
 }
-/// <summary>
-///
-/// </summary>
-void RenderOpening()
-{
-    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-    if(!handle) return;
 
-    std::cout << ".@@@@@@@@..@@......@@........@@....@@.......@@...########" << std::endl;
-    std::cout << ".@@....@@..@@......@@......@@@@....@@@@.....@@....##......" << std::endl;
-    std::cout << ".@@....@@..@@@@....@@........@@....@@#.###..@@....######.." << std::endl;
-    std::cout << ".@@....@@..@@@@....@@......@@@@....@@#......@@...##......" << std::endl;
-    std::cout << ".@@....@@..@@......@@........@@....@@.......@@...##......" << std::endl;
-    std::cout << ".@@@@@@@@..@@......@@@@@@@@..@@....@@.......@@...##......" << std::endl;
-    std::cout << "...........@@................@@....@@.......@@...########" << std::endl;
-
-
-
-}
 void startGame() {
- //   CONSOLE_CURSOR_INFO cursorInfo = { 0, }; // 커서 관련 정보 구조체
- //   cursorInfo.bVisible = 0; // 0 이면 커서 숨김, 1이면 커서 보임
- //   cursorInfo.dwSize = 1; // 커서 크기 1~100
- //   SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo); // 핸들 불러서 커서 
- //
-	//CONSOLE_SCREEN_BUFFER_INFO csbi; // 콘솔 화면 버퍼 정보 구조체
- //   GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
- //   /*COORD dwSize;                // 콘솔 버퍼의 크기 (가로, 세로)
- //   COORD dwCursorPosition;     // 현재 커서 위치 (X, Y)
- //   WORD  wAttributes;           // 현재 문자 색상 속성
- //   SMALL_RECT srWindow;        // 콘솔 창의 크기 (왼쪽, 위, 오른쪽, 아래)
- //   COORD dwMaximumWindowSize;  // 콘솔 창의 최대 크기 (가로, 세로)  << 구조체 멤버들임*/
- //   global::consoleScreenSize.Left = csbi.srWindow.Left; // 버퍼의 크기로 콘솔 화면의 크기 설정 >> 0
- //   global::consoleScreenSize.Right = csbi.srWindow.Right; // 버퍼의 크기로 콘솔 화면의 크기 설정 >> 0
- //   global::consoleScreenSize.Bottom = csbi.srWindow.Bottom; // 버퍼의 크기로 콘솔 화면의 크기 설정 >> 169
- //   global::consoleScreenSize.Top = csbi.srWindow.Top; // 버퍼의 크기로 콘솔 화면의 크기 설정 >> 50
- //   /*printf("콘솔 창의 위치:\n");
- //   printf("왼쪽: %d\n", csbi.srWindow.Left);
- //   printf("위쪽: %d\n", csbi.srWindow.Top);
- //   printf("오른쪽: %d\n", csbi.srWindow.Right);
- //   printf("아래쪽: %d\n", csbi.srWindow.Bottom);  // 콘솔창 및 버퍼 크기 확인 둘이 동일함  */
- //   global::playerMovableRect.Left = global::consoleScreenSize.Left + 2; // 플레이어 이동 범위 제한
- //   global::playerMovableRect.Right = global::consoleScreenSize.Right - 2; // 플레이어 이동 범위 제한
- //   global::playerMovableRect.Bottom = global::consoleScreenSize.Bottom - 2; // 플레이어 이동 범위 제한
- //   global::playerMovableRect.Top = global::consoleScreenSize.Top + 2; // 플레이어 이동 범위 제한
- //   /*printf("왼쪽: %d\n", global::playerMovableRect.Left);
- //   printf("위쪽: %d\n", global::playerMovableRect.Top);
- //   printf("오른쪽: %d\n", global::playerMovableRect.Right);
- //   printf("아래쪽: %d\n", global::playerMovableRect.Bottom);*/
-
-    
-
- //   DrawMovableRect(); // 테두리 벽 생성
-    DrawHomeRect(); // 집 벽 생성
-    DrawAtkRect(); //  공격력 강화소 벽 생성
-    DrawHpRect(); // 체력 강화소 벽 생성
-    DrawGoldRect(); //광산 벽 생성 
-    //DrawpoltalRect(); // 던전 입구 벽 생성
-    DrawDungeonRect();
-
-    global::prePlayerPos.X = 10;
-    global::prePlayerPos.Y = 10;
-    global::curPlayerPos.X = 10;
-    global::curPlayerPos.Y = 10; // 플레이어 시작 위치
-
-    DrawPlayer(false); // 플레이어 생성
-}
-/// <summary>
-/// </summary>게임 플레이 전 오프닝 화면
-/// <returns></returns>
-void OpeningTitle()
-{
     CONSOLE_CURSOR_INFO cursorInfo = { 0, }; // 커서 관련 정보 구조체
     cursorInfo.bVisible = 0; // 0 이면 커서 숨김, 1이면 커서 보임
     cursorInfo.dwSize = 1; // 커서 크기 1~100
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo); // 핸들 불러서 커서 
 
-    CONSOLE_SCREEN_BUFFER_INFO csbi; // 콘솔 화면 버퍼 정보 구조체
+	CONSOLE_SCREEN_BUFFER_INFO csbi; // 콘솔 화면 버퍼 정보 구조체
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
     /*COORD dwSize;                // 콘솔 버퍼의 크기 (가로, 세로)
     COORD dwCursorPosition;     // 현재 커서 위치 (X, Y)
@@ -460,37 +390,38 @@ void OpeningTitle()
     printf("오른쪽: %d\n", global::playerMovableRect.Right);
     printf("아래쪽: %d\n", global::playerMovableRect.Bottom);*/
 
+    
+
     DrawMovableRect(); // 테두리 벽 생성
+    DrawHomeRect(); // 집 벽 생성
+    DrawAtkRect(); //  공격력 강화소 벽 생성
+    DrawHpRect(); // 체력 강화소 벽 생성
+    DrawGoldRect(); //광산 벽 생성 
+    //DrawpoltalRect(); // 던전 입구 벽 생성
+    DrawDungeonRect();
 
+    global::prePlayerPos.X = 10;
+    global::prePlayerPos.Y = 10;
+    global::curPlayerPos.X = 10;
+    global::curPlayerPos.Y = 10; // 플레이어 시작 위치
+
+    DrawPlayer(false); // 플레이어 생성
 }
-
 int main()
 {
     global::time::InitTime(); // 시간 초기화
-
-    OpeningTitle();//오프닝 화면
-   
-   //오프닝 루프
-    while (IsGameRun())
+    startGame(); //게임 시작, 기본 화면 구성
+    
+    
+    while (IsGameRun()) // 게임 루프
     {
         global::time::UpdateTime();
+        
         ProcessInput();
         FixedUpdate();
-        RenderOpening();
+        Update();
+        Render();
     }
-
-    //startGame(); //게임 시작, 기본 화면 구성
-    //
-    //
-    //while (IsGameRun()) // 게임 루프
-    //{
-    //    global::time::UpdateTime();
-    //    
-    //    ProcessInput();
-    //    FixedUpdate();
-    //    Update();
-    //    Render();
-    //}
 
    
     return 0;
